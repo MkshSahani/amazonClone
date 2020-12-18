21756ac04c17833e9c3bb7f01c2b6d1fe4a531c0
MkshSahani <msco18333@gmail.com>
2020-12-18 17:04:31 +0530

    codes updated

---
+++ amazon/ShopDataBase/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ShopdatabaseConfig(AppConfig):
+    name = 'ShopDataBase'
--- amazon/amazon/settings.py
+++ amazon/amazon/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'amazonShop', # amazon shop registered. 
+    'ShopDataBase', # shop database added. 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
