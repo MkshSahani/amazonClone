a89b9be9f332dc5fd8b115cb4cc3313ad515ce72
MkshSahani <msco18333@gmail.com>
2021-01-09 00:43:17 +0530

    Prouduct Data Model added

--- amazon/ShopDataBase/admin.py
+++ amazon/ShopDataBase/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SellerModel 
+from .models import SellerModel, Product
 
-
-admin.site.register(SellerModel) # register Seller Model
+admin.site.register(SellerModel) # register Seller Model. 
+admin.site.register(Product) # register product Model. 
--- amazon/ShopDataBase/models.py
+++ amazon/ShopDataBase/models.py
@@ -11,3 +11,23 @@ class SellerModel(models.Model):  # Model Name : SellerModel.
     
     def __str__(self): 
         return str(self.seller_uid) + "_" + self.seller_name + "_" + self.seller_country # return the data. 
+
+
+
+# data Model Product. 
+
+class Product(models.Model):  # Model Name : Product. 
+    product_uid = models.IntegerField(primary_key=True) # unique ID of product. 
+    product_name = models.CharField(max_length=100) # name of product. 
+    seller_id = models.ForeignKey(related_name="Product_Seller", on_delete=models.PROTECT) # foreign key refer to seller.  
+    price = models.FloatField() # price in /100 scaled. 
+    
+
+    def __str__(self): 
+        return str(self.product_uid) + "_" + self.product_name # show it as label. 
+
+
+# data Modle ProductReview. 
+
+class ProductReview(models.Model): # Model Name : Prouduct Review. 
+    pass 
