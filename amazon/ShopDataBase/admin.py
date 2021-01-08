f725ed6ba1c7b818a2ab9760d7e945ef3f0e96d9
MkshSahani <msco18333@gmail.com>
2021-01-09 00:34:46 +0530

    Seller Added

--- amazon/ShopDataBase/admin.py
+++ amazon/ShopDataBase/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import SellerModel 
 
-# Register your models here.
+
+admin.site.register(SellerModel) # register Seller Model
--- amazon/ShopDataBase/models.py
+++ amazon/ShopDataBase/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+# data Model sellers. 
+
+class SellerModel(models.Model):  # Model Name : SellerModel. 
+    seller_uid = models.AutoField(primary_key=True) # autincremented ID. 
+    seller_name = models.CharField(max_length = 100) # seller Name. 
+    seller_country = models.CharField(max_length=100) # country of seller. 
+    seller_productType = models.CharField(max_length=100) # store the list in space seprated data. 
+    
+    def __str__(self): 
+        return str(self.seller_uid) + "_" + self.seller_name + "_" + self.seller_country # return the data. 
